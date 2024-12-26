4f2828fed7e3d44f7c3e5257226db6438637c87e
Devilman24 <156146049+Devilman24@users.noreply.github.com>
2024-12-26 04:26:57 +0100

    Update mac.py

--- mac.py
+++ mac.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 def get_mac_vendor(mac_address):
     url = f"https://api.macvendors.com/{mac_address}"
